02e58d1a70a7377232e6371f707e04b2309d9901
JeremiasSimian <jeresimian12@gmail.com>
2025-05-30 15:57:50 -0300

    Todo arreglado

---
+++ src/main/java/Controlador/GuiaControlador.java
@@ -0,0 +1,14 @@
+package Controlador;
+
+import BaseDatos.GuiaDB;
+import java.util.ArrayList;
+
+public class GuiaControlador {
+    
+    GuiaDB guiaDB;
+    
+    
+    public ArrayList buscarDNI(String dni){        
+           return  guiaDB.buscarDNI(dni);
+    }
+}
